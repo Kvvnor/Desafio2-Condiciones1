1117f0763a0ee3a85f1e418afbf02bd03065da4e
Kevin Ortiz <kvvinortiz@gmail.com>
2025-04-20 19:59:50 -0400

    Aplico selección de elementos del DOM

---
+++ assets/js/script2.js
@@ -0,0 +1,9 @@
+let input1 = document.querySelector(".input1");
+let input2 = document.querySelector(".input2");
+let input3 = document.querySelector(".input3");
+let button = document.querySelector(".btn-verify");
+let result = document.querySelector(".result");
+    
+
+
+

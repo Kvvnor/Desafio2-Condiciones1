e3cd1ae8cfbdffa054baadacd58bf7476528963b
Kevin Ortiz <kvvinortiz@gmail.com>
2025-04-20 00:01:09 -0400

    Agrego condicional else

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ imagen.addEventListener ("click", function() {
     if (imagen.style.border) {
         (imagen.style.border = "");
     }
+    
     else {
         (imagen.style.border = "2px solid red");
     }

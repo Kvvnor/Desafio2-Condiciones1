07d66d6a5e2b40bb37e3dd9d54b072782adbfb18
Kevin Ortiz <kvvinortiz@gmail.com>
2025-04-21 15:23:18 -0400

    Agrego un evento a la propiedad button

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -3,7 +3,11 @@ let input2 = document.querySelector(".input2");
 let input3 = document.querySelector(".input3");
 let button = document.querySelector(".btn-verify");
 let result = document.querySelector(".result");
-    
 
+
+// Agrego un evento al botón    
+button.addEventListenener("click", () => {
+
+})
 
 

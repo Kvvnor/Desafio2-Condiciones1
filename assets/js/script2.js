d8f9bc78d200b3b342aa9468fdbb6970cc5dc821
Kevin Ortiz <kvvinortiz@gmail.com>
2025-04-21 20:00:28 -0400

    Corrijo múltiples errores y llego al resultado final

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,19 +1,24 @@
-let input1 = document.querySelector(".input1");
-let input2 = document.querySelector(".input2");
-let input3 = document.querySelector(".input3");
-let button = document.querySelector(".btn-verify");
-let result = document.querySelector(".result");
+let input1 = document.querySelector('.input1');
+let input2 = document.querySelector('.input2');
+let input3 = document.querySelector('.input3');
+let button = document.querySelector('.btn-verify');
+let result = document.querySelector('.event');
 
 
 // Agrego un evento al botón    
-button.addEventListenener("click", () => {
+button.addEventListener('click', () => {
 
-    let value1 = input1.value;
-    let value2 = input2.value;
-    let value3 = input3.value;
+    let val1 = parseInt(input1.value) || 0;
+    let val2 = parseInt(input2.value) || 0;
+    let val3 = parseInt(input3.value) || 0;
 
-    let result = value1 + value2 + value3;
+    let total = val1 + val2 + val3;
 
-})
+// Agrego mensaje según el total de stickers
+    if (total <= 10) {
+        result.textContent = "Llevas " + total + " stickers";
+    }   else {
+        result.textContent = "Llevas demasiados stickes";
+    }
 
-
+});

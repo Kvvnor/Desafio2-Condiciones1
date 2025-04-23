b93687e50ad5cd780b7f8f05be0903ab4fcdddb8
Kevin Ortiz <kvvinortiz@gmail.com>
2025-04-23 16:02:36 -0400

    Creo las variables y me dirijo a los selectores con querySelector

---
+++ assets/js/script3.js
@@ -0,0 +1,5 @@
+let option1 = document.querySelector('.select1');
+let option2 = document.querySelector('.select2');
+let option3 = document.querySelector('.select3');
+let button = document.querySelector('.btn');
+let result = document.querySelector('.event');
